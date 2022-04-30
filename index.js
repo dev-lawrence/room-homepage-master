bf34e0c23bf3fdedc499beadefa964f82c2f6f74
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-04-30 21:51:32 +0100

    finished the js code for the hero slider

--- index.js
+++ index.js
@@ -2,6 +2,45 @@ const menuBar = document.querySelector('.menu');
 const headerContent = document.querySelector('.header__content-nav');
 const logo = document.querySelector('.logo');
 const navMenu = document.querySelector('.nav__menu');
+const slider = document.querySelector('.slider');
+const sliderContainer = document.querySelector('.slider__container');
+const slides = Array.from(sliderContainer.children);
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const slideSize = slider.offsetWidth;
+let currentSlide = 0;
+
+// move the slides
+const moveSlide = () => {
+  sliderContainer.style.transform = `translateX(-${
+    currentSlide * slideSize
+  }px)`;
+};
+
+// move to next slide
+const nextSlide = () => {
+  currentSlide++;
+  if (currentSlide > slides.length - 1) {
+    currentSlide = 0;
+  }
+
+  moveSlide();
+};
+
+// move to previous slide
+const prevSlide = () => {
+  currentSlide--;
+
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+
+  moveSlide();
+};
+
+// Event listeners for sliders
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
 
 // toggle menu on click
 menuBar.addEventListener('click', () => {
