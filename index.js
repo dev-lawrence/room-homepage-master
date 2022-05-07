a3ef039f2bd4942c2f820c45ce199bf6c84322af
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-05-07 23:00:33 +0100

    demo changes

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const sliderContainer = document.querySelector('.slider__container');
 const slides = Array.from(sliderContainer.children);
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
-const slideSize = slider.offsetWidth;
+// const slideSize = slider.offsetWidth;
+const slideSize = slider.getBoundingClientRect().width;
 let currentSlide = 0;
 const hero = document.querySelector('.hero');
 const heroContainer = document.querySelector('.hero__container');
 const heroText = Array.from(heroContainer.children);
 const body = document.querySelector('body');
 const overLay = document.querySelector('.overlay');
+
+console.log(slideSize);
 
 // MENU
 const menuFunction = () => {
@@ -37,108 +40,54 @@ navLinks.forEach((link) => {
   });
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-  // MOVE SLIDE //
-  const moveSlide = () => {
-    sliderContainer.style.transform = `translateX(-${
-      currentSlide * slideSize
-    }px)`;
-  };
+// MOVE SLIDE //
+const moveSlide = () => {
+  sliderContainer.style.transform = `translateX(-${
+    currentSlide * slideSize
+  }px)`;
+};
 
-  // move to next slide
-  const nextSlide = () => {
-    currentSlide++;
-    if (currentSlide > slides.length - 1) {
-      currentSlide = 0;
-    }
+// move to next slide
+const nextSlide = () => {
+  currentSlide++;
+  if (currentSlide > slides.length - 1) {
+    currentSlide = 0;
+  }
 
-    // logic to change texts
-    const active = document.querySelector('.active');
-    active.classList.remove('active');
+  // logic to change texts
+  const active = document.querySelector('.active');
+  active.classList.remove('active');
 
-    if (active.nextElementSibling) {
-      active.nextElementSibling.classList.add('active');
-    } else {
-      heroText[0].classList.add('active');
-    }
+  if (active.nextElementSibling) {
+    active.nextElementSibling.classList.add('active');
+  } else {
+    heroText[0].classList.add('active');
+  }
 
-    moveSlide();
-  };
+  moveSlide();
+};
 
-  // move to previous slide
-  const prevSlide = () => {
-    currentSlide--;
+// move to previous slide
+const prevSlide = () => {
+  currentSlide--;
 
-    if (currentSlide < 0) {
-      currentSlide = slides.length - 1;
-    }
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
 
-    // logic to change texts
-    const active = document.querySelector('.active');
-    active.classList.remove('active');
+  // logic to change texts
+  const active = document.querySelector('.active');
+  active.classList.remove('active');
 
-    if (active.previousElementSibling) {
-      active.previousElementSibling.classList.add('active');
-    } else {
-      heroText[heroText.length - 1].classList.add('active');
-    }
+  if (active.previousElementSibling) {
+    active.previousElementSibling.classList.add('active');
+  } else {
+    heroText[heroText.length - 1].classList.add('active');
+  }
 
-    moveSlide();
-  };
+  moveSlide();
+};
 
-  // Event listeners for sliders
-  nextBtn.addEventListener('click', nextSlide);
-  prevBtn.addEventListener('click', prevSlide);
-});
-
-// // MOVE SLIDE //
-// const moveSlide = () => {
-//   sliderContainer.style.transform = `translateX(-${
-//     currentSlide * slideSize
-//   }px)`;
-// };
-
-// // move to next slide
-// const nextSlide = () => {
-//   currentSlide++;
-//   if (currentSlide > slides.length - 1) {
-//     currentSlide = 0;
-//   }
-
-//   // logic to change texts
-//   const active = document.querySelector('.active');
-//   active.classList.remove('active');
-
-//   if (active.nextElementSibling) {
-//     active.nextElementSibling.classList.add('active');
-//   } else {
-//     heroText[0].classList.add('active');
-//   }
-
-//   moveSlide();
-// };
-
-// // move to previous slide
-// const prevSlide = () => {
-//   currentSlide--;
-
-//   if (currentSlide < 0) {
-//     currentSlide = slides.length - 1;
-//   }
-
-//   // logic to change texts
-//   const active = document.querySelector('.active');
-//   active.classList.remove('active');
-
-//   if (active.previousElementSibling) {
-//     active.previousElementSibling.classList.add('active');
-//   } else {
-//     heroText[heroText.length - 1].classList.add('active');
-//   }
-
-//   moveSlide();
-// };
-
-// // Event listeners for sliders
-// nextBtn.addEventListener('click', nextSlide);
-// prevBtn.addEventListener('click', prevSlide);
+// Event listeners for sliders
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
