3374afdf247214089c9c74c85e61ae651eb16b91
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-05-09 20:33:29 +0100

    added dom content loaded to the js file

--- index.js
+++ index.js
@@ -1,96 +1,187 @@
-const menuBar = document.querySelector('.menu');
-const headerContent = document.querySelector('.header__content-nav');
-const logo = document.querySelector('.logo');
-const navMenu = document.querySelector('.nav__menu');
-const navLinks = Array.from(navMenu.children);
-const slider = document.querySelector('.slider');
-const sliderContainer = document.querySelector('.slider__container');
-const slides = Array.from(sliderContainer.children);
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
-const img = document.querySelector('.h');
-const showWidth = img.offsetWidth;
-const slideSize = sliderContainer.offsetWidth;
-// const slideSize = slider.getBoundingClientRect().width;
-let currentSlide = 0;
-const hero = document.querySelector('.hero');
-const heroContainer = document.querySelector('.hero__container');
-const heroText = Array.from(heroContainer.children);
-const body = document.querySelector('body');
-const overLay = document.querySelector('.overlay');
+window.addEventListener('DOMContentLoaded', () => {
+  const menuBar = document.querySelector('.menu');
+  const headerContent = document.querySelector('.header__content-nav');
+  const logo = document.querySelector('.logo');
+  const navMenu = document.querySelector('.nav__menu');
+  const navLinks = Array.from(navMenu.children);
+  const slider = document.querySelector('.slider');
+  const sliderContainer = document.querySelector('.slider__container');
+  const slides = Array.from(sliderContainer.children);
+  const nextBtn = document.querySelector('.next');
+  const prevBtn = document.querySelector('.prev');
+  const slideSize = sliderContainer.offsetWidth;
+  let currentSlide = 0;
+  const hero = document.querySelector('.hero');
+  const heroContainer = document.querySelector('.hero__container');
+  const heroText = Array.from(heroContainer.children);
+  const body = document.querySelector('body');
+  const overLay = document.querySelector('.overlay');
 
-console.log(slideSize);
-console.log(showWidth);
+  console.log(slideSize);
 
-// MENU
-const menuFunction = () => {
-  menuBar.classList.toggle('active');
-  headerContent.classList.toggle('active');
-  logo.classList.toggle('active');
-  navMenu.classList.toggle('active');
-  body.classList.toggle('show');
-  overLay.classList.toggle('show');
-};
+  // MENU
+  const menuFunction = () => {
+    menuBar.classList.toggle('active');
+    headerContent.classList.toggle('active');
+    logo.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    body.classList.toggle('show');
+    overLay.classList.toggle('show');
+  };
 
-// toggle menu on click
-menuBar.addEventListener('click', () => {
-  menuFunction();
-});
-
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
+  // toggle menu on click
+  menuBar.addEventListener('click', () => {
     menuFunction();
   });
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      menuFunction();
+    });
+  });
+
+  // MOVE SLIDE //
+  const moveSlide = () => {
+    sliderContainer.style.transform = `translateX(-${
+      currentSlide * slideSize
+    }px)`;
+  };
+
+  // move to next slide
+  const nextSlide = () => {
+    currentSlide++;
+    if (currentSlide > slides.length - 1) {
+      currentSlide = 0;
+    }
+
+    // logic to change texts
+    const active = document.querySelector('.active');
+    active.classList.remove('active');
+
+    if (active.nextElementSibling) {
+      active.nextElementSibling.classList.add('active');
+    } else {
+      heroText[0].classList.add('active');
+    }
+
+    moveSlide();
+  };
+
+  // move to previous slide
+  const prevSlide = () => {
+    currentSlide--;
+
+    if (currentSlide < 0) {
+      currentSlide = slides.length - 1;
+    }
+
+    // logic to change texts
+    const active = document.querySelector('.active');
+    active.classList.remove('active');
+
+    if (active.previousElementSibling) {
+      active.previousElementSibling.classList.add('active');
+    } else {
+      heroText[heroText.length - 1].classList.add('active');
+    }
+
+    moveSlide();
+  };
+
+  // Event listeners for sliders
+  nextBtn.addEventListener('click', nextSlide);
+  prevBtn.addEventListener('click', prevSlide);
 });
 
-// MOVE SLIDE //
-const moveSlide = () => {
-  sliderContainer.style.transform = `translateX(-${
-    currentSlide * slideSize
-  }px)`;
-};
+// const menuBar = document.querySelector('.menu');
+// const headerContent = document.querySelector('.header__content-nav');
+// const logo = document.querySelector('.logo');
+// const navMenu = document.querySelector('.nav__menu');
+// const navLinks = Array.from(navMenu.children);
+// const slider = document.querySelector('.slider');
+// const sliderContainer = document.querySelector('.slider__container');
+// const slides = Array.from(sliderContainer.children);
+// const nextBtn = document.querySelector('.next');
+// const prevBtn = document.querySelector('.prev');
+// const slideSize = sliderContainer.offsetWidth;
+// let currentSlide = 0;
+// const hero = document.querySelector('.hero');
+// const heroContainer = document.querySelector('.hero__container');
+// const heroText = Array.from(heroContainer.children);
+// const body = document.querySelector('body');
+// const overLay = document.querySelector('.overlay');
 
-// move to next slide
-const nextSlide = () => {
-  currentSlide++;
-  if (currentSlide > slides.length - 1) {
-    currentSlide = 0;
-  }
+// console.log(slideSize);
 
-  // logic to change texts
-  const active = document.querySelector('.active');
-  active.classList.remove('active');
+// // MENU
+// const menuFunction = () => {
+//   menuBar.classList.toggle('active');
+//   headerContent.classList.toggle('active');
+//   logo.classList.toggle('active');
+//   navMenu.classList.toggle('active');
+//   body.classList.toggle('show');
+//   overLay.classList.toggle('show');
+// };
 
-  if (active.nextElementSibling) {
-    active.nextElementSibling.classList.add('active');
-  } else {
-    heroText[0].classList.add('active');
-  }
+// // toggle menu on click
+// menuBar.addEventListener('click', () => {
+//   menuFunction();
+// });
 
-  moveSlide();
-};
+// navLinks.forEach((link) => {
+//   link.addEventListener('click', () => {
+//     menuFunction();
+//   });
+// });
 
-// move to previous slide
-const prevSlide = () => {
-  currentSlide--;
+// // MOVE SLIDE //
+// const moveSlide = () => {
+//   sliderContainer.style.transform = `translateX(-${
+//     currentSlide * slideSize
+//   }px)`;
+// };
 
-  if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
+// // move to next slide
+// const nextSlide = () => {
+//   currentSlide++;
+//   if (currentSlide > slides.length - 1) {
+//     currentSlide = 0;
+//   }
 
-  // logic to change texts
-  const active = document.querySelector('.active');
-  active.classList.remove('active');
+//   // logic to change texts
+//   const active = document.querySelector('.active');
+//   active.classList.remove('active');
 
-  if (active.previousElementSibling) {
-    active.previousElementSibling.classList.add('active');
-  } else {
-    heroText[heroText.length - 1].classList.add('active');
-  }
+//   if (active.nextElementSibling) {
+//     active.nextElementSibling.classList.add('active');
+//   } else {
+//     heroText[0].classList.add('active');
+//   }
 
-  moveSlide();
-};
+//   moveSlide();
+// };
 
-// Event listeners for sliders
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
+// // move to previous slide
+// const prevSlide = () => {
+//   currentSlide--;
+
+//   if (currentSlide < 0) {
+//     currentSlide = slides.length - 1;
+//   }
+
+//   // logic to change texts
+//   const active = document.querySelector('.active');
+//   active.classList.remove('active');
+
+//   if (active.previousElementSibling) {
+//     active.previousElementSibling.classList.add('active');
+//   } else {
+//     heroText[heroText.length - 1].classList.add('active');
+//   }
+
+//   moveSlide();
+// };
+
+// // Event listeners for sliders
+// nextBtn.addEventListener('click', nextSlide);
+// prevBtn.addEventListener('click', prevSlide);
