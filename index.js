a5f544e1940937de7754045eb91db95f9a6947b4
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-05-11 20:12:56 +0100

    refactored the code

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const reloadUsingLocationHash = () => {
+const reloadUsingLocationHash = (yea) => {
   window.location.hash = 'reload';
 };
-window.onload = reloadUsingLocationHash();
+window.onload = reloadUsingLocationHash(true);
 
 const menuBar = document.querySelector('.menu');
 const headerContent = document.querySelector('.header__content-nav');
 const logo = document.querySelector('.logo');
 const navMenu = document.querySelector('.nav__menu');
 const navLinks = Array.from(navMenu.children);
-// const slider = document.querySelector('.slider');
-// const sliderContainer = document.querySelector('.slider__container');
-// const slides = Array.from(sliderContainer.children);
+const carousel = document.querySelector('.carousel');
+const carouselSlides = Array.from(carousel.children);
+const imgLength = carouselSlides.length;
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
-// const slideSize = sliderContainer.offsetWidth;
-// let currentSlide = 0;
+const slideSize = carouselSlides[0].clientWidth;
+let currentSlide = 0;
 const hero = document.querySelector('.hero');
 const heroContainer = document.querySelector('.hero__container');
 const heroText = Array.from(heroContainer.children);
 const body = document.querySelector('body');
 const overLay = document.querySelector('.overlay');
-
-// console.log(slideSize);
+console.log(slideSize);
 
 // MENU
 const menuFunction = () => {
@@ -44,66 +43,7 @@ navLinks.forEach((link) => {
   });
 });
 
-// MOVE SLIDE //
-// const moveSlide = () => {
-//   sliderContainer.style.transform = `translateX(-${
-//     currentSlide * slideSize
-//   }px)`;
-// };
-
-// move to next slide
-// const nextSlide = () => {
-//   currentSlide++;
-//   if (currentSlide > slides.length - 1) {
-//     currentSlide = 0;
-//   }
-
-//   // logic to change texts
-//   const active = document.querySelector('.active');
-//   active.classList.remove('active');
-
-//   if (active.nextElementSibling) {
-//     active.nextElementSibling.classList.add('active');
-//   } else {
-//     heroText[0].classList.add('active');
-//   }
-
-//   moveSlide();
-// };
-
-// move to previous slide
-// const prevSlide = () => {
-//   currentSlide--;
-
-//   if (currentSlide < 0) {
-//     currentSlide = slides.length - 1;
-//   }
-
-//   // logic to change texts
-//   const active = document.querySelector('.active');
-//   active.classList.remove('active');
-
-//   if (active.previousElementSibling) {
-//     active.previousElementSibling.classList.add('active');
-//   } else {
-//     heroText[heroText.length - 1].classList.add('active');
-//   }
-
-//   moveSlide();
-// };
-
-// Event listeners for sliders
-// nextBtn.addEventListener('click', nextSlide);
-// prevBtn.addEventListener('click', prevSlide);
-
 // CAROUSEL DEMO
-const carousel = document.querySelector('.carousel');
-const carouselSlides = Array.from(carousel.children);
-const imgLength = carouselSlides.length;
-const slideSize = carouselSlides[0].clientWidth;
-let currentSlide = 0;
-console.log(slideSize);
-
 // set up the slider
 const reset = () => {
   carouselSlides.forEach((slide, i) => {
